eb54fae2294dbdd64394599bbc180c27c4bcf38c
Diego Arango <crash.diego91@gmail.com>
2024-04-18 15:52:19 -0500

    Added admin

--- patients/admin.py
+++ patients/admin.py
@@ -1,3 +1,15 @@
+"""Patients models admin."""
+
+# Django
 from django.contrib import admin
 
-# Register your models here.
+# Custom models
+from patients.models import Patient
+
+
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    """Patient model admin."""
+    list_display = ('personal_id', 'first_name', 'last_name')
+    search_fields = ('personal_id', 'first_name', 'last_name', 'email')
+    list_filter = ('personal_id', 'first_name', 'last_name')
