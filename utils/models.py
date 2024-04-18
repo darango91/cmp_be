0ca0cc47f8e23d0893367ff825430a33e34963a2
Diego Arango <crash.diego91@gmail.com>
2024-04-18 12:31:12 -0500

    Black linter

--- utils/models.py
+++ utils/models.py
@@ -2,6 +2,7 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+
 class CMPModel(models.Model):
     """
     Cuidate mas pro base model.
@@ -14,24 +15,25 @@ class CMPModel(models.Model):
     """
 
     created = models.DateTimeField(
-        'created at',
+        "created at",
         auto_now_add=True,
-        help_text=_('Date time on which the object was created.')
+        help_text=_("Date time on which the object was created."),
     )
     modified = models.DateTimeField(
-        'modified at',
+        "modified at",
         auto_now=True,
-        help_text=_('Date time on which the object was last modified.')
+        help_text=_("Date time on which the object was last modified."),
     )
 
     class Meta:
         """
         Meta options
         """
+
         abstract = True
 
-        get_latest_by = 'created'
-        ordering = ['-created', '-modified']
+        get_latest_by = "created"
+        ordering = ["-created", "-modified"]
 
 
 class CMPHumanModel(CMPModel):
@@ -46,10 +48,8 @@ class CMPHumanModel(CMPModel):
 
     personal_id = models.IntegerField(
         unique=True,
-        error_messages={
-            'unique': _('A user with the same personal ID already exists')
-        },
-        primary_key=True
+        error_messages={"unique": _("A user with the same personal ID already exists")},
+        primary_key=True,
     )
 
     address = models.CharField(max_length=40)
@@ -57,15 +57,13 @@ class CMPHumanModel(CMPModel):
     state = models.CharField(max_length=40)
     country = models.CharField(max_length=40)
 
-    phone_number = models.CharField(
-        max_length=17,
-        blank=True
-    )
+    phone_number = models.CharField(max_length=17, blank=True)
 
     class Meta:
         """
         Meta options
         """
+
         abstract = True
 
-        ordering = ['-personal_id']
+        ordering = ["-personal_id"]
